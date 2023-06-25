c6852cfc28f52685aa9615df6248ba257ab05461
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-06-25 21:27:13 +0800

    add chat

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import Chat from 'components/Chat'
 
-export default function IndexPage() {
+export default function Page() {
+
   return (
-    <div className="flex h-48 items-center justify-center bg-gray-100 font-sans text-cyan-700">
-      <div className="text-center text-4xl font-bold">A demo page</div>
-    </div>
+    <Chat />
   )
 }
