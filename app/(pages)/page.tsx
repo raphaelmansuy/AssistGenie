b6a9d8d20f2fae48fe5a750ff16b0f1432cc44f5
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-06-27 14:43:46 +0800

    update

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,5 +1,7 @@
 import Chat from '@/components/chat'
 
+
+
 export default function Page() {
   return <Chat />
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,10 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ['var(--font-sans)', ...fontFamily.sans],
+        fontFamily: {
+          sans: ['var(--font-inter)'],
+          mono: ['var(--font-roboto-mono)'],
+        },
       },
       colors: {
         border: 'hsl(var(--border))',
