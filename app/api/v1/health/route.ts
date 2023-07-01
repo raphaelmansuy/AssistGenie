a48e4d51f14c93bad5f23127ed1aa429abd6805a
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-07-01 09:53:53 +0800

    update

--- app/api/v1/health/route.ts
+++ app/api/v1/health/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
   return NextResponse.json({
-    status: 'ok',
+    status: 'OK',
     message: 'Hello from the API',
     // formay: 'YYYY-MM-DDTHH:mm:ss.sssZ',
     timeUTC: new Date().toISOString(),
