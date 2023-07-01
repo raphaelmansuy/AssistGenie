bc0df47c56946465494e6831842a1db58338909b
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2023-07-01 22:15:32 +0800

    update

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -25,7 +25,7 @@ const portalVariants = cva('fixed inset-0 z-50 flex', {
 
 interface SheetPortalProps
   extends SheetPrimitive.DialogPortalProps,
-  VariantProps<typeof portalVariants> { }
+    VariantProps<typeof portalVariants> {}
 
 const SheetPortal = ({
   position,
@@ -144,7 +144,7 @@ const sheetVariants = cva(
 
 export interface DialogContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-  VariantProps<typeof sheetVariants> { }
+    VariantProps<typeof sheetVariants> {}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
